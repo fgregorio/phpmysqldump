5db9f676a639a7bc4d0670bcb266a39a24766eb1
Felipe <bean@bsolucoes.com.br>
2016-06-09 10:45:59 -0300

    no message

---
+++ phpmysqldump.php
@@ -0,0 +1,63 @@
+<?php
+
+if (php_sapi_name() == "cli") {
+
+    $config = array(
+        'mysql' => array(
+			'h' => 'localhost'
+			,'u' => 'root'
+			,'B' => 'mysql'
+		)
+		,'test' => array(
+			'h' => 'localhost'
+			,'u' => 'root'
+			,'B' => 'test'
+		)
+    );
+
+    $file = 'dump/'.date('Y.m.d.H.i.s').'.sql';
+	
+	if((!isset($config)) || sizeof($config)==0){
+		die(PHP_EOL."Please edit \$config var");
+	}
+
+    echo "Choise your option, between 1 and ".sizeof($config);
+	
+	$i = 1;
+	foreach($config as $key => $val){	
+		echo PHP_EOL.($i)." -> ".($key);
+		$i++;
+	}
+
+    echo PHP_EOL."Your choice: ";
+    echo PHP_EOL;
+
+    $handle = fopen ("php://stdin","r");
+    $choice = fgets($handle);
+	
+	$i = 1;
+	foreach($config as $item){	
+		if($i == intval($choice)){
+			$cmd = 'mysqldump ';
+			foreach($item as $key => $val){
+				if($key == 'p'){
+					$cmd .= ' -'.$key.$val;
+				}
+				else {
+					$cmd .= ' -'.$key.' '.$val;
+				}				
+			}
+			exec($cmd.' > '.$file);			
+			break;
+		}	
+		$i++;
+	}
+	
+    if(file_exists($file)){
+        echo PHP_EOL."Ok, file: ";
+        echo PHP_EOL.$file;
+    }
+
+    echo PHP_EOL;
+
+}
